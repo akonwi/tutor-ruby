6fd122ac9cf378177fe3364b3993b7e42c6391ca
akonwi <akonwi@gmail.com>
2013-10-27 17:46:52 -0500

    Centering the form for new words. Figured out clearing of elements that stick around. Need to extract that into a private method

--- lib/app.rb
+++ lib/app.rb
@@ -14,10 +14,24 @@ class StudyBuddy < Shoes
 
   WORDS = []
 
-  # store currently used stacks
+  # store containers
   APP = Hashie::Mash.new
 
   def index
+    # TODO: Extract these two if statements into private methods
+    if APP.edit_lines?
+      puts "clearing edit_lines"
+      APP.edit_lines.map &:remove
+      APP.edit_lines = nil
+    end
+
+    if APP.buttons?
+      APP.buttons.each do |key, val|
+        puts "Removing #{key} button"
+        val.remove
+      end
+    end
+
     # For some reason, filling the window with this stack causes
     # an overflow and needs a scrollbar. .92 is the perfect fit
     APP[:main] = stack height: 0.92 do
@@ -26,7 +40,10 @@ class StudyBuddy < Shoes
 
       flow do
         main_background
-        button 'study' do
+
+        # This button doesn't get removed when APP.main gets cleared
+        # so it needs to be manually removed. Storing it for reference
+        APP.study_button = button 'study' do
           visit '/study'
         end
         button 'add vocab' do
@@ -37,58 +54,67 @@ class StudyBuddy < Shoes
   end
 
   def add_words
+    APP.study_button.remove
+
     APP.main.clear do
-        stack margin: 10 do
-          main_background curve: 20
-          banner 'New Word', align: 'center', margin_bottom: 5
+      stack do
+        main_background
+        banner 'New Word', align: 'center', margin_bottom: 5
 
-          # Collection of edit_lines for enumerability
-          edit_lines = {inf: nil,
-                        def: nil,
-                        je: nil,
-                        tu: nil,
-                        il: nil,
-                        nous: nil,
-                        vous: nil,
-                        ils: nil}
+        # Collection of edit_lines for enumerability
+        edit_lines = {inf: nil,
+                      def: nil,
+                      je: nil,
+                      tu: nil,
+                      il: nil,
+                      nous: nil,
+                      vous: nil,
+                      ils: nil}
 
-          flow do
-            para "Infinitve: "
-            edit_lines[:inf] = edit_line
-            para "Definition: "
-            edit_lines[:def] = edit_line
-          end
+        # trying to center the form with the margin_left
+        flow margin_left: 200 do
+          para "Infinitve: "
+          edit_lines[:inf] = edit_line
+          para "Definition: "
+          edit_lines[:def] = edit_line
+        end
 
-          # Setup inputs and labels for conjugations
-          edit_lines.each_key do |key|
-            next if key.eql?(:inf) || key.eql?(:def)
-            flow do
-              para "#{key.to_s}: "
-              edit_lines[key] = edit_line
-            end
-          end
-
-          flow do
-            button "Save" do
-              @word = Word.new do |word|
-                edit_lines.each do |k,v|
-                  word[k] = v.text
-                end
-              end
-
-              WORDS << @word
-              clear_edit_lines edit_lines.values
-            end
-
-            button 'New Word' do
-              clear_edit_lines edit_lines.values
-            end
-
-            button "Go Back" do
-              visit '/'
-            end
+        # Setup inputs and labels for conjugations
+        edit_lines.each_key do |key|
+          next if key.eql?(:inf) || key.eql?(:def)
+          flow margin_left: 200 do
+            para "#{key.to_s}: "
+            edit_lines[key] = edit_line
           end
         end
+
+        # storing the edit_lines from the form for removal later
+        APP.edit_lines = edit_lines.values
+
+        flow margin_left: 200 do
+          APP.buttons!.save = button "Save" do
+            # TODO: verify edit_lines aren't empty
+            @word = Word.new do |word|
+              edit_lines.each do |k,v|
+                word[k] = v.text
+              end
+            end
+
+            puts "Created #{@word.inf}"
+
+            WORDS << @word
+            clear_edit_lines edit_lines.values
+          end
+
+          APP.buttons.new = button 'New Word' do
+            clear_edit_lines edit_lines.values
+          end
+
+          APP.buttons.back = button "Go Back" do
+            visit '/'
+          end
+        end
+      end
     end
   end
 
@@ -99,7 +125,7 @@ class StudyBuddy < Shoes
     word = WORDS[index]
 
     stack height: 0.8, width: 1.0 do
-      @title = span "#{word.inf}", align: "center"
+      @title = banner "#{word.inf}", align: "center"
     end
 
     flow margin: 10 do
@@ -128,4 +154,4 @@ class StudyBuddy < Shoes
     end
 end
 
-Shoes.app title: 'Study Buddy', height: 400
+Shoes.app title: 'Study Buddy', height: 500
