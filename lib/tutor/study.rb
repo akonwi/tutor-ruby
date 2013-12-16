f6c6d75008fc0f14463e7f6b671eb4cb7b77373b
akonwi <akonwi@gmail.com>
2013-12-16 01:58:12 -0600

    Finished with setup for studying verbs

--- lib/tutor/study.rb
+++ lib/tutor/study.rb
@@ -43,25 +43,109 @@ module Tutor
 
       @@words.shuffle!
 
-      visit "/study/#{0}"
+      study_word(0)
     end
 
     def study_word(index)
       clean
 
       # conjugations and definition keys
-      stuff_to_ask = [:def, :je, :tu, :il, :nous, :vous, :ils]
+      @stuff_to_ask = [:def, :je, :tu, :il, :nous, :vous, :ils]
 
-      word = @@words[index]
+      @word = @@words[index]
+      @@words_index = index
 
       APP.main.clear do |key|
         stack Options do
-          banner word.inf, align: 'center'
-          ## TODO: Show inputs for the answer
-          # If it's a verb, need to show stuff one at a time
-          # else just show the definition input
+          main_background
+          if @word
+            banner = banner @word.inf, align: 'center'
+            if @word.type.eql? :verb
+              quiz_on_verb(0)
+            else
+              # TODO: define the 'quiz' method for regular words
+            end
+          else
+            banner "There's nothing to study", align: 'center'
+            APP.buttons!.back = button 'Back' do
+              visit '/'
+            end
+          end
         end
       end
     end
+
+    private
+
+      # @param index position in stuff_to_ask array. indicates which
+      #              step user is in
+      # this method will display the label and input for the verb being studied
+      def quiz_on_verb(index)
+        flow margin_left: 200 do
+          label = @stuff_to_ask[index]
+          if label == :def
+            @label = para 'Definition'
+          else
+            @label = para label
+          end
+
+          @input = edit_line
+          if lines = APP.edit_lines
+            lines << @input
+          else
+            APP.edit_lines = [@input]
+          end
+        end
+
+        flow margin: 30 do
+          APP.buttons!.back = button 'Back' do
+            visit '/'
+          end
+
+          APP.buttons.next = button 'Next' do
+            go_next index
+          end
+
+          # when 'enter' key is pressed
+          keypress do |key|
+            go_next(index) if key.eql? "\n"
+          end
+        end
+      end
+
+      # @param index current index of conjugation form in stuff_to_ask
+      #
+      # this is just a block for when the next button is clicked or
+      # enter key is pressed to do what I can to keep code DRY
+      def go_next(index)
+        if validate(index)
+          index += 1
+          next_conjugation(index)
+        else
+          puts 'Sorry, that is incorrect'
+        end
+      end
+
+      # @params index of the current word being studied in the array of words
+      #
+      # This method will change the label for the next verb form
+      # or
+      # it will prompt going to the next word if all forms have been exhausted
+      def next_conjugation(index)
+        if next_conj = @stuff_to_ask[index]
+          @label.text = next_conj
+          @input.text = ''
+        else
+          study_word(@@words_index + 1)
+        end
+      end
+
+      # @params index position inside the array of conjugations,
+      #               indicates which verb form is being asked
+      # @returns boolean is the entered text correct
+      def validate(index)
+        puts "answer is: " + @word[@stuff_to_ask[index]]
+        @input.text.eql? @word[@stuff_to_ask[index]]
+      end
   end
 end
